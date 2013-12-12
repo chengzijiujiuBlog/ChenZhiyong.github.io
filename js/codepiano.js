00f0ee78afe41b74a050a6343801146195a56b8d
ChenZhiyong <chenzhiyong115@gmail.com>
2013-12-12 15:55:12 +0800

    change name

--- js/codepiano.js
+++ js/codepiano.js
@@ -1,7 +1,7 @@
 /* =============================================================================
-#     FileName: codepiano.js
+#     FileName: codecatalyst.js
 #         Desc: javascript for blog
-#       Author: codepiano
+#       Author: codecatalyst
 #        Email: anyexingchen999@qq.com
 #     HomePage: http://www.weibo.com/anyexingchen
 #      Version: 0.0.1
